11b0361d4ef41b5feab6961d4ff5f8c339692303
johkli <johannes.klint@etraveligroup.com>
2020-05-06 10:41:46 +0200

    Add array functionality

--- README.md
+++ README.md
@@ -4,8 +4,35 @@ Module to link dependencies to static folder
 
 ## Usage
 
+Pass in a string as the first argument
+
+```jsx
+await linkDependencies("fs-extra", "./public/scripts");
+```
+
+Pass in an array as the first argument for copying multiple directories.
+
+```jsx
+await linkDependencies(["fs-extra", "chalk"], "./public/scripts");
+```
+---
+#### util function with try / catch
+```jsx
+async function copy() {
+  try {
+    await linkDependencies("fs-extra", "./public/scripts");
+  } catch (error) {
+    throw new Error("Noo", error);
+  }
+}
+…
+await copy();
+```
+
+#### then / catch
+
 ```jsx
 linkDependencies("fs-extra", "./public/scripts")
-  .then(() => console.log("Finished!"))
-  .catch(console.error);
+  .then(() => console.log("Yes!"))
+  .catch(() => console.error("Something went wrong!"));
 ```
--- index.js
+++ index.js
@@ -1,40 +1,65 @@
+const fs = require('fs-extra')
+
+function errorHandling(target, destination) {
+  if (typeof destination !== 'string') {
+    throw new Error(`Not a valid destination folder: ${destination}`);
+  }
+  if (typeof target !== 'string') {
+    if (Array.isArray(target)) {
+      return;
+    }
+    throw new Error(`Not a valid target folder(s): ${target}`);
+  }
+}
+
+function getTargets(target) {
+  if (Array.isArray(target)) {
+    return target;
+  }
+  return [target];
+}
+
 /**
  * 
  * @param {String} target dependency target  
  * @param {String} dest destination where the dependency(target) will end up. for example ./public/build
  * @returns {Promise} Will be resolved when the target has been copied
  */
-function linkDependencies(target, dest) {
-  if (typeof dest !== "string" || typeof target !== "string") {
-    throw new Error(`Not a valid destination folder: ${dest || target}`);
-  }
+function linkDependencies(target, destination) {
+  errorHandling(target, destination);
 
-  fs.mkdirpSync(dest);
-  const rLinkSource = path.join("./node_modules", target);
-  const linkSource = path.resolve(rLinkSource);
-  const rLinkDestiny = path.join(dest, target);
-  const linkDestiny = path.resolve(rLinkDestiny);
+  fs.mkdirpSync(destination);
 
-  return new Promise((resolve) => {
-    fs.access(linkDestiny, fs.F_OK, resolve);
-  })
-    .then(
-      (err) =>
-        new Promise((resolve) => {
-          if (err) {
-            resolve();
-          } else {
-            fs.unlink(linkDestiny, resolve);
-          }
-        })
-    )
-    .then(
-      () =>
-        new Promise((resolve) => {
-          fs.symlink(linkSource, linkDestiny, "dir", resolve);
-        })
-    )
-    .catch(console.error);
+  const promise = getTargets(target).map(depName => {
+    const rLinkSource = path.join('./node_modules', depName);
+    const rLinkDestiny = path.join(destination, depName);
+
+    const linkSource = path.resolve(rLinkSource);
+    const linkDestiny = path.resolve(rLinkDestiny);
+
+    return new Promise(resolve => {
+      fs.ensureDir(linkDestiny, resolve);
+    })
+      .then(
+        err =>
+          new Promise(resolve => {
+            if (err) {
+              resolve();
+            } else {
+              fs.remove(linkDestiny, resolve);
+            }
+          }),
+      )
+      .then(
+        () =>
+          new Promise(resolve => {
+            fs.symlink(linkSource, linkDestiny, 'dir', resolve);
+          }),
+      )
+      .catch(console.error);
+  });
+
+  return Promise.all(promise);
 }
 
 module.exports = linkDependencies;
